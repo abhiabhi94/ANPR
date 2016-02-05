f2872d7b923e3e929e82e28bd934800c3d257fc0
abhiabhi <abhyudai.pratap.singh@gmail.com>
2016-02-05 23:00:54 +0530

    New Test Images and Canny Edges detection added

--- numberplate.py
+++ numberplate.py
@@ -1,6 +1,6 @@
 import cv2
 import operator
-# import numpy as np
+import numpy as np
 
 MIN_CONTOUR_AREA = 300.0
 MAX_CONTOUR_AREA = 600.0
@@ -38,9 +38,10 @@ def main():
     PlatesContour=[]
     crossingContour=[]
 
-    img=cv2.resize(cv2.imread('car2.jpg'),(800,600))
+    img=cv2.resize(cv2.imread('car1.jpg'),(800,600))
     imgCopy=img.copy()
     img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgGrayCopy=img.copy()
     # imgBlurred = cv2.GaussianBlur(img, (5,5), 0)
     # cv2.imshow("Blurr",imgBlurred)
     ret,imgThresh = cv2.threshold(img,100,255,cv2.THRESH_BINARY_INV)     #for detecting car
@@ -100,10 +101,20 @@ def main():
     ret1,imgThresh1 = cv2.threshold(img,150,255,cv2.THRESH_BINARY_INV)   # for detecting crossing
     cv2.imshow('Thresh 1',imgThresh1)
     imgThresh1Copy= imgThresh1.copy()
-    npaContours1 , npaHierarchy = cv2.findContours(imgThresh1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    edge = cv2.Canny( imgCopy , 100 , 200 )
+    cv2.imshow("edges detected", imgThresh1 )
+    edgesInContour , edgesHierarchy  = cv2.findContours(edge , cv2.RETR_TREE , cv2.CHAIN_APPROX_SIMPLE )
+    # cv2.drawContours ( imgCopy , edgesInContour , -1 , (255,0,0) , 2 )
+    cv2.waitKey(0)
+    # mask = cv2.inRange ( imgCopy ,np.array ( [ 150,150,150 ] ),np.array ( [ 255,255,255 ] ) )
+    # print mask
+    # imgmask=cv2.bitwise_and ( imgCopy , imgCopy , mask=mask )
+    # cv2.imshow ( "Masked Image" , imgThresh1Copy )
+    # cv2.waitKey(0)
+    # npaContours1 , npaHierarchy = cv2.findContours(imgmask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     # cv2.drawContours(imgCopy,npaContours1,-1,(255,0,255),2)
 
-    for npaContour in npaContours1:
+    for npaContour in edgesInContour:
 
         contourWithData = ContourWithData()                                             # instantiate a contour with data object
         contourWithData.npaContour = npaContour                                         # assign contour to contour with data
@@ -118,12 +129,12 @@ def main():
 
         # if contourWithData.checkIfContourIsValid():
         ratio = float( intWidth ) / intHeight
-        if( ratio > 2  and ratio < 4 and contourWithData.fltArea > 1000.0 and contourWithData.fltArea < 1500.0 ):
+        if ( ratio > 2  and ratio < 4 and contourWithData.fltArea > 1000.0 and contourWithData.fltArea < 1500.0 ):
         # if contourWithData.checkIfContourIsValid():
             # contourWithData=np.array(contourWithData)
-            print contourWithData.fltArea , ratio
-            for i in xrange (len(contourWithData.npaContour)):
-                print contourWithData.npaContour[i]
+            print contourWithData.fltArea , ratio , contourWithData.npaContour , len ( contourWithData.npaContour )
+            # for i in xrange (len(contourWithData.npaContour)):
+            #     print contourWithData.npaContour[i]
                 # M = cv2.moments(contourWithData.npaContour[i])
                 # cx1 = int( M['m10'] / M['m00'] )
                 # cy1 = int( M['m01'] / M['m00'] )
@@ -141,7 +152,8 @@ def main():
     # X = x + crossingContour[-1]
     # Y = 
     # cv2.rectangle(imgCopy,() , (crossingContour[0].intRectX + 2 , ((crossingContour[-1].intRectX - crossingContour[0].intRectX) + crossingContour[-1].intWidth + 2 , (crossingContour[0].intRectY + 2 , ((crossingContour[-1].intRectY - crossingContour[0].intRectY) + crossingContour[-1].intWidth + 2 )
-    cv2.imshow("Crossings and number plate",imgCopy)
+    # cv2.imshow("Crossings and number plate",imgCopy)
+    cv2.imshow("Contours After Edge Detection", imgCopy )
     cv2.waitKey(0)
 
 
