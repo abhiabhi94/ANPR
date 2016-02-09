263fd0fcc528d6d350d7346616efa60b87cdadc9
abhiabhi <abhyudai.pratap.singh@gmail.com>
2016-02-09 21:15:03 +0530

    Morphological transformation and edian filter added into preprocessing

--- numberplate.py
+++ numberplate.py
@@ -30,8 +30,8 @@ class ContourWithData():
 
 	def checkIfContourIsValid(self):                            # contour selection
 		ratio = float( self.intRectWidth ) / self.intRectHeight
-		if ratio > 2  and ratio < 5 and (self.fltArea > MIN_CONTOUR_AREA and self.fltArea < MAX_CONTOUR_AREA) : return True        # much better validity checking would be necessary
-		return False
+		if ratio >= 2  and ratio <= 4 and (self.fltArea > MIN_CONTOUR_AREA and self.fltArea < MAX_CONTOUR_AREA) : return True        # much better validity checking would be necessary
+		return False 
 
 
 def preprocessing(img):
@@ -39,9 +39,13 @@ def preprocessing(img):
 	imgCopy=img.copy()
 	img=cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
 	imgGrayCopy=img.copy()
-	# imgBlur = cv2.GaussianBlur(img , (5,5) , 0)
-	edge = cv2.Canny (img , 100 , 255 )
-	return edge , imgGrayCopy , imgCopy
+	imgBlur = cv2.medianBlur(img , 1 , 0)
+	otsuReturn , imgThresh = cv2.threshold ( imgBlur , 0 , 255 , cv2.THRESH_OTSU)
+	imgThreshCopy = imgThresh.copy()
+	imgMorphed = cv2.morphologyEx(imgThresh , cv2.MORPH_OPEN , (5 , 5))
+	edge = cv2.Canny (imgBlur , 100 , 255 )
+	cv2.imshow("Number Plates detected", edge)
+	return edge, imgGrayCopy , imgCopy
 
 
 def main():
@@ -59,18 +63,6 @@ def main():
 		crossingContour = []
 		imgResized = cv2.resize(img , ( int(x[1]/resizingParameter) , int(x[0]/resizingParameter) ))
 		
-		# print counter
-		# if (counter <= 0):
-		#     print counter
-		#     return True
-
-		# counter -= 1
-
-		# img=cv2.resize(cv2.imread('car.jpg') , ((counter + 2 ) * 100 , counter * 100 ))
-		# cv2.imshow("Original Image" , img)
-		# imgCopy=img.copy()
-		# img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-		# imgGrayCopy=img.copy()
 		# imgBlurred = cv2.GaussianBlur(img, (5,5), 0)
 		# cv2.imshow("Blurr",imgBlurred)
 		# ret,imgThresh = cv2.threshold(img,100,255,cv2.THRESH_BINARY_INV)     #for detecting car
@@ -114,26 +106,9 @@ def main():
 
 		
 		edge , imgGrayCopy , imgCopy = preprocessing (imgResized)
-		# imgBlur = cv2.GaussianBlur(img , (5,5) , 0)
-		# ret1 , imgThresh1 = cv2.threshold(imgBlur,150,255,cv2.THRESH_BINARY_INV)   # for detecting crossing
-		# cv2.imshow('Thresh 1',imgThresh1)
-		# edge = cv2.Canny( imgBlur , 100 , 255 )
-		otsuReturn , imgThresh2 = cv2.threshold ( edge , 0 , 255 , cv2.THRESH_OTSU)
-		print otsuReturn
-		imgThresh2Copy = imgThresh2.copy()
-		cv2.imshow("edges detected", imgThresh2 )
-		edgesInContour , edgesHierarchy  = cv2.findContours ( imgThresh2 , cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
-		# cv2.drawContours ( imgCopy , edgesInContour , -1 , (255,0,0) , 2 )
-		# cv2.waitKey(0)
-		# mask = cv2.inRange ( imgCopy ,np.array ( [ 150,150,150 ] ),np.array ( [ 255,255,255 ] ) )
-		# print mask
-		# imgmask=cv2.bitwise_and ( imgCopy , imgCopy , mask=mask )
-		# cv2.imshow ( "Masked Image" , imgThresh1Copy )
-		# cv2.waitKey(0)
-		# npaContours1 , npaHierarchy = cv2.findContours(imgmask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-		# cv2.drawContours(imgCopy,npaContours1,-1,(255,0,255),2)
+		Contour , Hierarchy  = cv2.findContours ( edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE )
 
-		for npaContour in edgesInContour:
+		for npaContour in Contour:
 
 			contourWithData = ContourWithData()                                             # instantiate a contour with data object
 			contourWithData.npaContour = npaContour                                         # assign contour to contour with data
