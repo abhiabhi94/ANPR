cd41ad98e4b9432aad1f22e4b8844bde8d0455ec
Anupam Singh <anupamsingh610@gmail.com>
2016-02-19 19:35:52 +0530

    added plate classification and normalised data for k-nearest

--- font/fonts.py
+++ font/fonts.py
@@ -1,35 +1,34 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("font71.png",0)
+img = cv2.imread("font81.png",0)
 cv2.imshow("fd",img)
 cv2.waitKey(0)
 
-x = 6
-y = 25
+x = 14	
+y = 20
 
-width = 49
-height = 98
+width =49
+height = 100
 
-noOfCharacters = 14	
+noOfCharacters = 14
 for i in range(noOfCharacters):
-	# if( i == 3):
-	# 	h = height + 8
+	# if( i == 1):
+	# 	w = width - 30
 	# elif( i == 5):
 	# 	h = height + 8
 	# elif( i == 9):
 	# 	h = height + 8
 	# else:
 	# 	h = height
-	
+	# else:
 	w = width
 	# if(i == 6):
 	# 	# cv2.imwrite("sample/"+str(i*10 + 1)+".jpg",img[y-4	:h, x : x + w - 7])
 	# 	cv2.imwrite("sample/"+str(i)+"/5.png",cv2.resize(img[y-4:h, x : x + w - 7] , (28,28)))
 
-
-	# cv2.imwrite("sample/"+str(unichr(i+78))+"/6.png",cv2.resize(img[y:height, x : x + w - 7] , (28,28)))
-	# cv2.imwrite("sample/"+str(i)+"/4.png",cv2.resize(img[y:height, x : x + w] , (28,28)))
+	# cv2.imwrite("sample/"+str(unichr(i+78))+"/7.png",cv2.resize(img[y:height, x : x + w - 4] , (28,28)))
+	# cv2.imwrite("sample/"+str(i)+"/7.png",cv2.resize(img[y:height, x : x + w] , (28,28)))
 	cv2.imwrite("sample/"+str(i*10 + 1)+".jpg",img[y:height, x : x + w])
 
 
